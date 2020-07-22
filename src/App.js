bd7def1f2f7af97ea878c3169ed0f5b13662a241
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-23 01:18:19 +0530

    Created a simple Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Method 1 - Standard MEthod, It gets compiled to below method before execution
+    // Below code contains a jsx structure i.e it can have only 1 root element which is div element
+    <div className='App'>
+      <h1>Hi hi</h1>
+      <Person />
     </div>
+
+    // Method 2 - Old method
+    // React.createElement('div', null, 'Hi hi')
+
+    // Method 3 - Creating Nested element
+    // React.createElement('div', null, React.createElement('h1', null, 'Hihi'))
   );
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from "react";
+// ES6 stype
+const person = () => {
+    return <p> I'm a Person!</p>
+};
+
+export default person;
+// Older method
+// const person = function (){}
